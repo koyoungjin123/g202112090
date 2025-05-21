ff8a8a5dfca2fe9f93bba465ec80d376cb409bb5
ko youngjin <yjko0609@office.skhu.ac.kr>
2025-05-21 09:19:12 +0900

    feat: Add World.java with run(), run2()

---
+++ World.java
@@ -0,0 +1,9 @@
+public class World {
+    public static void run() {
+        System.out.println("2023154899");
+    }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
+}
